ce94db9893fde1e2526e95912fbd26c7bfb47425
AdrianCebulski2000 <adriancebulski@gmail.com>
2023-09-06 14:47:11 +0200

    Class of Players added

---
+++ PomeranianTransfermarkt/PomeranianTransfermarkt/Entities/Players.cs
@@ -0,0 +1,13 @@
+﻿namespace PomeranianTransfermarkt.Entities
+{
+    public class Players
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Surname { get; set; } = default!;
+        public int Age { get; set; } = default!;
+        public string Position { get; set; } = default!;
+        public double MarketValue { get; set; } = default!;
+        public string Nationality { get; set; } = default!;
+    }
+}
